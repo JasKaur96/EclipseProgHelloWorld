8e60fe60d2fd523d952f28a48d9f22ab71d6f076
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-07 13:46:04 +0530

    Added: Gradle Project in IntelliJ

---
+++ src/main/java/com/gradlepractice/SimpleGradle.java
@@ -0,0 +1,7 @@
+package com.gradlepractice;
+
+public class SimpleGradle {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my simple Gradle project.");
+    }
+}
